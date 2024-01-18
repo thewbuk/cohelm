30aa9de7f722a95e0ea94b443cc4f1e3d98fa961
Wojciech Bandzerewicz <57405495+thewbuk@users.noreply.github.com>
2024-01-18 17:02:56 +0000

    typo

--- components/Summary.tsx
+++ components/Summary.tsx
@@ -8,7 +8,7 @@ const Summary = ({ isMet }: { isMet: boolean }) => {
         className={`p-4 rounded-lg shadow-md text-white ${isMet ? 'bg-green-500' : 'bg-red-500/80'} flex justify-between items-center`}
       >
         {isMet ? 'Condition met' : 'Condition not met'}
-        <Button variant="outline">
+        <Button variant="outline" className="text-black dark:text-white">
           <Check className="h4 w-4 mr-2" />
           Submit
         </Button>
